5392cb7b9243498ee758142d542635b65248b232
braulio1309 <brauliozapatad@gmail.com>
2026-02-01 22:53:29 -0400

    aa

--- application/config/whatsapp.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['whatsapp_token'] = 'EAATZAz13IzbQBQL4WApNbSpprJfXAFjcZAsSpbZAffghKOHXplLP10BAD1LJwQjcpe348Q2wkw7u9nxPbuUodAMVa1wWdkJO68mq9VLKxGtZCEMuXeyEbZBQOUDrbWWzChBvTHETaZAldpzwmM6ORiX3sskwT5arc3jq99Wapu9w0SrpRf9hyCZBHI8MPDLpqkv6AZDZD';
-$config['identificador_numero_telefonico'] = '808278662362668';	// 3102196923
-
-// $config['identificador_numero_telefonico'] = '';	// 3102196923 (Test)
-$config['identificador_cuenta_whatsapp_business '] = '2592661617733236';	// 3102196923 (Principal)
